7d5ca8597d2da30ab9bfccf444fec61a45bd69b3
Udo Kastilan <uk@skaliant.de>
2015-08-20 22:49:02 +0200

    First commit

---
+++ uk-wax-test/src/main/java/de/skaliant/waxtest/ctrl/IndexController.java
@@ -0,0 +1,37 @@
+package de.skaliant.waxtest.ctrl;
+
+import javax.servlet.http.HttpServletRequest;
+
+import de.skaliant.wax.results.Result;
+
+/**
+ * 
+ *
+ * @author Udo Kastilan
+ */
+public class IndexController
+{
+	private String what = null;
+	
+	
+	public String index()
+	{
+		return "index";
+	}
+	
+	
+	public Object answer(HttpServletRequest req)
+	{
+		if (what != null)
+		{
+			req.setAttribute("what", what);
+		}
+		return Result.redirect(getClass(), "index").build();
+	}
+	
+	
+	public void setWhat(String what)
+	{
+		this.what = what;
+	}
+}
